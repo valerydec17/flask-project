1e54152d0d61cf8d688aeb4bb1005b9f9032a6ae
Valera <valerydec17@gmail.com>
2018-05-15 20:56:34 +0700

    Commit4

---
+++ data.py
@@ -0,0 +1,26 @@
+def Articles():
+	articles = [
+		{
+			'id': 1,
+			'title': 'Article One',
+			'body': 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry''s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.',
+			'author': 'Brad Traversy',
+			'create_date':'04-25-2017'  
+		},
+		{
+			'id': 2,
+			'title': 'Article Two',
+			'body': 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry''s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.',
+			'author': 'John Doe',
+			'create_date':'04-25-2017'  
+		},
+		{
+			'id': 3,
+			'title': 'Article Three',
+			'body': 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry''s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.',
+			'author': 'Brad Traversy',
+			'create_date':'04-25-2017'  
+		}
+
+	]
+	return articles
