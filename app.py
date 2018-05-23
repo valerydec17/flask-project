7e3d450323a4e22f48049dbe0be69daa0e689a60
Valera <valerydec17@gmail.com>
2018-05-23 19:16:20 +0700

    lesson 4 finisged: some features don't work, fix in future

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, flash, redirect, url_for, session, request, logging
-from data import Articles
+#from data import Articles
 from flask_mysqldb import MySQL 
 from wtforms import Form, StringField, TextAreaField, PasswordField, validators
 from passlib.hash import sha256_crypt
@@ -17,7 +17,7 @@ app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
 # init MySQL
 mysql = MySQL(app)
 
-Articles = Articles() 
+#Articles = Articles() 
 
 # Index
 @app.route('/')
@@ -33,12 +33,37 @@ def about():
 # Articles
 @app.route('/articles')
 def articles():
-	return render_template('articles.html', articles = Articles)
+
+	# Create cursor
+	cur = mysql.connection.cursor()
+
+	# Get articles
+	result = cur.execute("SELECT * FROM articles")
+
+	articles = cur.fetchall()
+
+	if result > 0:
+		return render_template('articles.html', articles=articles)
+	else:
+		msg = 'No Articles Found'
+		return render_template('articles.html', msg=msg)
+	# Close connection
+	cur.close()
+
+
 
 # Single Article
 @app.route('/article/<string:id>/')
 def article(id):
-	return render_template('article.html', id=id)
+	# Create cursor
+	cur = mysql.connection.cursor()
+
+	# Get article
+	result = cur.execute("SELECT * FROM articles WHERE id=%s", [id])
+
+	articles = cur.fetchone()
+
+	return render_template('article.html', article=article)
 
 # Register Form Class
 class RegisterForm(Form):
@@ -147,7 +172,23 @@ def logout():
 @app.route('/dashboard')
 @is_logged_in
 def dashboard():
-	return render_template('dashboard.html')
+	# Create cursor
+	cur = mysql.connection.cursor()
+
+	# Get articles
+	result = cur.execute("SELECT * FROM articles")
+
+	articles = cur.fetchall()
+
+	if result > 0:
+		return render_template('dashboard.html', articles=articles)
+	else:
+		msg = 'No Articles Found'
+		return render_template('dashboard.html', msg=msg)
+	# Close connection
+	cur.close()
+
+	
 
 
 # Article Form Class
@@ -181,7 +222,7 @@ def add_article():
 
 		return redirect(url_for('dashboard'))
 
-	return render_template('add_article', form=form)
+	return render_template('add_article.html', form=form)
 
 
 
